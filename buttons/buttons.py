1753ef7d0fdd0f322f6f850fddfe10f81a36ec26
zmqp <domarlaid2@mail.ru>
2023-04-24 02:28:31 +0500

    Add user keyboard

--- buttons/buttons.py
+++ buttons/buttons.py
@@ -1,13 +1,24 @@
 from aiogram.types import *
 
 # Buttons
-authenticate_button = KeyboardButton("Начать")
-skip_auth_button = KeyboardButton("Продолжить без авторизации")
+authenticate_button = KeyboardButton("🚻 Начать")
+skip_auth_button = KeyboardButton("🚷 Продолжить без авторизации")
+record_to_appointment_button = KeyboardButton("📋 Записаться к врачу")
+ask_a_question_button = KeyboardButton("❔ Задать вопрос")
+information_button = KeyboardButton("ℹ️ Информация")
+get_support_button = KeyboardButton("☎️ Обратиться в поддержку")
+help_button = KeyboardButton("📍 Помощь")
+settings_button = KeyboardButton("🛠 Настройки")
 
 # Keyboards
 welcome_keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(authenticate_button, skip_auth_button)
 admin_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 employee_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-user_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+user_main_keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(record_to_appointment_button,
+                                                                   ask_a_question_button,
+                                                                   information_button,
+                                                                   get_support_button,
+                                                                   help_button,
+                                                                   settings_button)
 
 
--- handlers/general.py
+++ handlers/general.py
@@ -1,11 +1,23 @@
 from aiogram import types, Dispatcher
 from utils.database.questions import get_answer
-from buttons.buttons import welcome_keyboard
+from buttons.buttons import welcome_keyboard, user_main_keyboard
 
 
 async def start(message: types.Message):
     await message.answer(get_answer('start'), reply_markup=welcome_keyboard)
 
 
+async def skip_auth(message: types.Message):
+    await message.answer('Вы проигнорировали авторизацию.'
+                         ' Без авторизации множество функций ограничено')
+    await message.answer(get_answer('user_help'), reply_markup=user_main_keyboard, parse_mode="Markdown")
+
+
+async def help_command(message: types.Message):
+    await message.answer(get_answer('user_help'), reply_markup=welcome_keyboard, parse_mode="Markdown")
+
+
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(start, commands=['start'])
+    dp.register_message_handler(skip_auth, lambda message: message.text == '🚷 Продолжить без авторизации')
+    dp.register_message_handler(help_command, commands=['help'])
